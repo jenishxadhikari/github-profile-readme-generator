eeb792585d5c0f0536dd3bd1b45ae43783338a3b
jenishxadhikari <114239379+jenishxadhikari@users.noreply.github.com>
2025-01-30 21:29:55 +0545

    chore: setup initial project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function App() {
+  return (
+    <main className="flex flex-col items-center justify-center gap-3 py-5">
+      <h1 className="text-3xl font-bold">Github Profile Readme Generator</h1>
+      <Button>Hello World</Button>
+    </main>
+  )
+}
